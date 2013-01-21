59943d0200e366be459129bdaae73c8398500058
Andy Cai <huayicai@gmail.com>
2013-01-22 01:48:50 +0800

    removed 3rdparty dir

--- 3rdparty/luassert/stub.lua
+++
@@ -1,7 +0,0 @@
--- module will return a single stub function, no table nor register any assertions
-local spy = require 'luassert.spy'
-
-return function(self, key, func)
-  self[key] = spy.new(func)
-  return self[key]
-end
--- 3rdparty/lunatest/test-error_handler.lua
+++
@@ -1,9 +0,0 @@
-require "lunatest"
-
--- This should error out with a more meaningful error message
--- than "./lunatest.lua:608: attempt to index local 'e' (a function value)".
-function test_foo()
-   error(function() return 2 end)
-end
-
-lunatest.run()
