1da6b1595425cb31782f6e53bc151a248b735cd9
Andy Cai <huayicai@gmail.com>
2013-01-22 19:39:06 +0800

    Moved function Route to init.lua

--- main.lua
+++ main.lua
@@ -6,29 +6,6 @@ function __G__TRACKBACK__(msg)
     print("----------------------------------------")
 end
 
--- public interface for C++
--- response: may be a json string or other you want
--- parse reponse and dispatch to real module
-function Route(response)
-	xpcall(function()
-		if response then
-			local resp = kode.json.decode(response)
-			local act = string.explode(resp.act, ".")
-			local service = kode.getglobal(act[1].."Service")
-			local action = act[2]
-			if service ~= nil then
-				if service[action] then
-					service[action](service, resp.param)
-				else
-					log4l.debug("need a function [%s] in service [%s]", action, act[1])
-				end
-			else
-				log4l.debug("Wrong aciton: %s", response)
-			end
-		end
-	end, __G__TRACKBACK__)
-end
-
 local function main()
 	require "kode.init"		-- framework
 	require "init"			-- app
--- protected/init.lua
+++ protected/init.lua
@@ -1,9 +1,33 @@
 if kode == nil then return end
 
+-- C++(network) interface for lua 
 if kode.getglobal("RequestServer") == nil then
     function RequestServer( ... )
         -- nothing to do
     end
+end
+
+-- public interface for C++
+-- response: may be a json string or other you want
+-- parse reponse and dispatch to real module
+function Route(response)
+	xpcall(function()
+		if response then
+			local resp = kode.json.decode(response)
+			local act = string.explode(resp.act, ".")
+			local service = kode.getglobal(act[1].."Service")
+			local action = act[2]
+			if service ~= nil then
+				if service[action] then
+					service[action](service, resp.param)
+				else
+					log4l.debug("need a function [%s] in service [%s]", action, act[1])
+				end
+			else
+				log4l.debug("Wrong aciton: %s", response)
+			end
+		end
+	end, __G__TRACKBACK__)
 end
 
 kode.appendPkgPath("./thirdparty/quick-cocos2d-x/")
