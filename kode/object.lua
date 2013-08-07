a479fb44c83da5b4ce0eab662981f4ed616018ca
wayee <iwayee@gmail.com>
2013-08-07 17:07:35 +0800

    added test code for object.lua

--- kode/object.lua
+++ kode/object.lua
@@ -13,3 +13,31 @@ end
 meta.new = meta.extend
 
 kode.object = meta
+
+--[[ 
+--sample:
+
+local base = meta:extend()
+
+base.key1 = "base.key1"
+base.key2 = "base.key2"
+
+local a = base:extend({})
+local b = base:extend({})
+local c = base:extend({})
+
+a.key1 = "a.key1"
+b.key1 = "b.key1"
+c.key1 = "c.key1"
+
+print("base.key1: " .. base["key1"])
+print("base.key2: " .. base["key2"])
+
+print("a.key1: " .. a["key1"])
+print("b.key1: " .. b["key1"])
+print("c.key1: " .. c["key1"])
+
+print("a.key2: " .. a["key2"])
+print("b.key2: " .. b["key2"])
+print("c.key2: " .. c["key2"])
+--]]
