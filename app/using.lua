e099b37bc997c816909bb74790186c6c5a546252
wayee <iwayee@gmail.com>
2013-08-25 18:16:11 +0800

    updated using.lua

--- app/using.lua
+++ app/using.lua
@@ -1,9 +1,4 @@
 if kode == nil then return end
 
-using.quick_cocos2d_x = nil
-
-if using.quick_cocos2d_x ~= nil then
-	kode.appendPkgPath("./thirdparty/quick-cocos2d-x/")
-	require("framework.init")
-	require("framework.client.init")
-end
+-- require("framework.init")
+-- require("framework.client.init")
