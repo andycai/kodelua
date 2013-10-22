f71fccf4f7e44ac095a0c947c4f0dc13794a8aa9
wayee <iwayee@gmail.com>
2013-10-22 19:47:31 +0800

    added math.randseed

--- kode/ext/math.lua
+++ kode/ext/math.lua
@@ -2,7 +2,6 @@ function math.round(num)
     return math.floor(num + 0.5)
 end
 
-function math.random2(...)
+function math.randseed()
     math.randomseed(tonumber(tostring(os.time()):reverse():sub(1,6)))
-    return math.random(...)
 end
